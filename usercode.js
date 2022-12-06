0dbc410e03b97cefdff530172026cdf6cbc652cb
Tatianaaaaaaa <118683978+Tatianaaaaaaa@users.noreply.github.com>
2022-12-06 21:37:19 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,4 +1,8 @@
-header = { "alg": "HS256", "typ": "JWT"};
+const SECRET_KEY = 'cAtwa1kkEy';
+const unsignedToken = base64urlEncode(header) + '.' + base64urlEncode(payload);
+const signature = HMAC-SHA256(unsignedToken, SECRET_KEY);
+
+//header = { "alg": "HS256", "typ": "JWT"};
 
 /*module.exports = (data) => {
     data.hello = "Hello World!";
