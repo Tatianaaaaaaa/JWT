fe49f15b6d501c51c9d5c99ae8daf847cc8ab8af
Tatianaaaaaaa <118683978+Tatianaaaaaaa@users.noreply.github.com>
2022-12-07 00:05:21 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,16 +1,47 @@
+var jws = require('jws');
 
-module.exports = (data) => {
+module.exports = function (jwt, options) {
+  options = options || {};
+  var decoded = jws.decode(jwt, options);
+  if (!decoded) { return null; }
+  var payload = decoded.payload;
+
+  //try parse the payload
+  if(typeof payload === 'string') {
+    try {
+      var obj = JSON.parse(payload);
+      if(obj !== null && typeof obj === 'object') {
+        payload = obj;
+      }
+    } catch (e) { }
+  }
+
+  //return header if `complete` option is enabled.  header includes claims
+  //such as `kid` and `alg` used to select the key within a JWKS needed to
+  //verify the signature
+  if (options.complete === true) {
+    return {
+      header: decoded.header,
+      payload: payload,
+      signature: decoded.signature
+    };
+  }
+  return payload;
+};
+
+
+/*module.exports = (data) => {
    /*function utf8_to_b64(str) {
       return window.btoa(unescape(encodeURIComponent(str)));
     }*/
    
-   function b64EncodeUnicode(str) {
+  /* function b64EncodeUnicode(str) {
   return btoa(encodeURIComponent(str));
    }
     data.res = b64EncodeUnicode("✓ à la mode");
     
-    return data;
-};
+    return data;*/
+};*/
 
 
 
