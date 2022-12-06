e91fd3cfc0b13703c697e6f1e86de31d0c74d608
Tatianaaaaaaa <118683978+Tatianaaaaaaa@users.noreply.github.com>
2022-12-06 19:27:24 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,11 +1,6 @@
 module.exports = (data) => 
 
-  header = { "alg": "HS256", "typ": "JWT"}  ;
-  payload = { "userId": "b08f86af-35da-48f2-8fab-cef3904660bd" } ;
-  const SECRET_KEY = 'cAtwa1kkEy';
-  const unsignedToken = base64urlEncode(header) + '.' + base64urlEncode(payload);
-  const signature = HMAC-SHA256(unsignedToken, SECRET_KEY);
-  data.token = encodeBase64Url(header) + '.' + encodeBase64Url(payload) + '.' + encodeBase64Url(signature);
+ npm install jsonwebtoken
   
 
     return data;
