dec10392740bac572ee935360719dfa345b07a13
Tatianaaaaaaa <118683978+Tatianaaaaaaa@users.noreply.github.com>
2022-12-06 21:40:23 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -1,6 +1,14 @@
-const SECRET_KEY = 'cAtwa1kkEy';
+const sha1 = require("crypto-js/sha1");
+
+module.exports = (data) => {
+    data.res = sha1(data.in).toString(); 
+    
+    return data;
+};
+
+/*const SECRET_KEY = 'cAtwa1kkEy';
 const unsignedToken = base64urlEncode(header) + '.' + base64urlEncode(payload);
-const signature = HMAC-SHA256(unsignedToken, SECRET_KEY);
+const signature = HMAC-SHA256(unsignedToken, SECRET_KEY);*/
 
 //header = { "alg": "HS256", "typ": "JWT"};
 
