c64ca8318f01c7880391f2e1b5c167904f2e8b77
Tatianaaaaaaa <118683978+Tatianaaaaaaa@users.noreply.github.com>
2022-12-07 23:21:21 +0200

    Update usercode.js

--- usercode.js
+++ usercode.js
@@ -3,4 +3,3 @@ var jwt = require('../index');
 var expect = require('chai').expect;
 var jws = require('jws');
 var PS_SUPPORTED = require('../lib/psSupported');
-const {generateKeyPairSync} = require("crypto");
